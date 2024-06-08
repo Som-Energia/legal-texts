65a12670a0578a31ba940f3a822462ba4dfdf047
David García Garzón <david.garcia@somenergia.coop>
2024-06-08 20:45:14 +0200

    ✨ diffing previous template, multiple files
    - When generating templates diff the previous one
    - All commands accept multiple files

--- legal_text_processor.py
+++ legal_text_processor.py
@@ -7,6 +7,7 @@ import re
 import typer
 import itertools
 from consolemsg import warn, step, error
+import difflib
 
 help="""\
 Extracts a monolingual translation yaml from a markdown of a legal text,
@@ -70,47 +71,69 @@ def ensure_extension(path: Path, extension: str):
     if path.suffix == extension: return
     raise Exception(f"Expected {extension} extension")
 
+def extract_block_ids(blocks: list[list[str]]):
+    block_ids = ['PRE']
+    for block in blocks:
+        new_id = extract_id(block)
+        if not new_id: continue
+        active_id = unrepeated_id(block_ids, new_id)
+        block_ids.append(active_id)
+    return block_ids
+
+def diff(template_file: Path, content: list[str]):
+    oldcontent = []
+    if template_file.exists():
+        with template_file.open() as f:
+            oldcontent = f.readlines()
+    difflines = difflib.unified_diff(
+        oldcontent,
+        content,
+        fromfile=str(template_file),
+        tofile='new',
+    )
+    return ''.join(difflines)
+
 app = typer.Typer(
     help=help,
 )
 
 @app.command()
-def extract(markdown_file: Path):
-    print(f"extracting {markdown_file}")
-    ensure_extension(markdown_file, '.md')
-    blocks = analyze_blocks(markdown_file.open())
-    result = extract_translations(blocks)
-    result.dump(markdown_file.with_suffix(".yaml"))
+def extract(markdown_file: list[Path]):
+    for md_file in markdown_file:
+        print(f"Extracting {md_file}")
+        ensure_extension(md_file, '.md')
+        blocks = analyze_blocks(md_file.open())
+        result = extract_translations(blocks)
+        result.dump(md_file.with_suffix(".yaml"))
 
 @app.command()
-def template(markdown_file: Path):
-    print(f"extracting {markdown_file}")
-    ensure_extension(markdown_file, '.md')
-    blocks = analyze_blocks(markdown_file.open())
+def template(markdown_file: list[Path]):
+    for md_file in markdown_file:
+        step(f"Extracting template from {md_file}")
+        ensure_extension(md_file, '.md')
+        template_file = md_file.parent / 'template.md'
 
-    block_ids = ['PRE']
-    for block in blocks:
-        new_id = extract_id(block)
-        if not new_id: continue
-        step(new_id)
-        active_id = unrepeated_id(block_ids, new_id)
-        block_ids.append(active_id)
-
-    template_file = markdown_file.parent / 'template.md'
-    step(f"Writing {template_file}...")
-    template_file.write_text('\n'.join((
-        f"{{{id}}}" for id in block_ids
-    )))
+        blocks = analyze_blocks(md_file.open())
+        block_ids = extract_block_ids(blocks)
+        content = [
+            f"{{{id}}}\n" for id in block_ids
+        ]
+        content_diff = diff(template_file, content)
+        if content_diff: warn(content_diff)
+        step(f"Writing {template_file}...")
+        template_file.write_text(''.join(content))
 
 @app.command()
-def generate(translation_yaml: Path):
-    ensure_extension(translation_yaml, '.yaml')
-    translation_yaml.with_suffix('.md').write_text(content)
-    print(f"Generating {}")
-    translation = ns.load(translation_yaml)
-    template = (translation_yaml.parent/'template.md').read_text()
-    content = template.format(**translation)
-    translation_yaml.with_suffix('.md').write_text(content)
+def generate(translation_yaml: list[Path]):
+    for yaml_file in translation_yaml:
+        ensure_extension(yaml_file, '.yaml')
+        markdown_file = yaml_file.with_suffix('.md')
+        template_file = yaml_file.parent/'template.md'
+        step(f"Generating {markdown_file} from {yaml_file} and {template_file}")
+        translation = ns.load(yaml_file)
+        template = (yaml_file.parent/'template.md').read_text()
+        content = template.format(**translation)
+        markdown_file.write_text(content)
 
 if __name__ == "__main__":
     app()
